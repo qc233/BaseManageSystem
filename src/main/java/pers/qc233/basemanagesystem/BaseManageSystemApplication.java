cb86ca05b857a94f7fe144553957acd047f8fb5b
qc233 <17639858537@163.com>
2022-11-23 12:26:51 +0800

    first commit

---
+++ src/main/java/pers/qc233/basemanagesystem/BaseManageSystemApplication.java
@@ -0,0 +1,13 @@
+package pers.qc233.basemanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseManageSystemApplication.class, args);
+    }
+
+}
