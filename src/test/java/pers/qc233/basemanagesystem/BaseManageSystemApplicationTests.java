41b8db0b428134ef714886eaca4cada24ae859fa
qc233 <17639858537@163.com>
2022-11-23 14:58:04 +0800

    构建第一个窗口

---
+++ src/test/java/pers/qc233/basemanagesystem/BaseManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.qc233.basemanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
