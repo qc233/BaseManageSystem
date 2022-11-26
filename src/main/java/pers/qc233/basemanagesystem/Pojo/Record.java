459a2a89739b3d44bbd161cf8e1de8c45ad6fb81
qc233 <17639858537@163.com>
2022-11-26 14:46:49 +0800

    实现信息页面的排行榜按钮和退出按钮的功能

--- src/main/java/pers/qc233/basemanagesystem/Pojo/Record.java
+++ src/main/java/pers/qc233/basemanagesystem/Pojo/Record.java
@@ -1,10 +1,12 @@
 package pers.qc233.basemanagesystem.Pojo;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.sql.Timestamp;
 
 @Data
+@TableName("record")
 public class Record {
     int id;
     String username;
